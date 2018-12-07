3c87e47b107a695fde3b29366f7ce5603753cb45
Sercan Turkmen <sercanturkmen@outlook.com>
2018-12-07 13:09:51 +0200

    some extra files

---
+++ nets/unet_classic.py
@@ -0,0 +1,145 @@
+
+from keras.layers import Conv2D, MaxPooling2D, Dropout, UpSampling2D, concatenate
+
+import keras.backend as backend
+import keras.layers as layers
+from keras_applications.mobilenet_v2 import _make_divisible, correct_pad
+
+def inverted_res_block(inputs, expansion, stride, filters, block_id):
+    alpha = 1.0
+
+    in_channels = backend.int_shape(inputs)[-1]
+    pointwise_conv_filters = int(filters * alpha)
+    pointwise_filters = _make_divisible(pointwise_conv_filters, 8)
+    x = inputs
+    prefix = 'block_{}_'.format(block_id)
+
+    if block_id:
+        # Expand
+        x = layers.Conv2D(expansion * in_channels,
+                          kernel_size=1,
+                          padding='same',
+                          use_bias=False,
+                          activation=None,
+                          name=prefix + 'expand')(x)
+        x = layers.BatchNormalization(epsilon=1e-3,
+                                      momentum=0.999,
+                                      name=prefix + 'expand_BN')(x)
+        x = layers.ReLU(6., name=prefix + 'expand_relu')(x)
+    else:
+        prefix = 'expanded_conv_'
+
+    # Depthwise
+    if stride == 2:
+        x = layers.ZeroPadding2D(padding=correct_pad(backend, x, 3),
+                                 name=prefix + 'pad')(x)
+    x = layers.DepthwiseConv2D(kernel_size=3,
+                               strides=stride,
+                               activation=None,
+                               use_bias=False,
+                               padding='same' if stride == 1 else 'valid',
+                               name=prefix + 'depthwise')(x)
+    x = layers.BatchNormalization(epsilon=1e-3,
+                                  momentum=0.999,
+                                  name=prefix + 'depthwise_BN')(x)
+
+    x = layers.ReLU(6., name=prefix + 'depthwise_relu')(x)
+
+    # Project
+    x = layers.Conv2D(pointwise_filters,
+                      kernel_size=1,
+                      padding='same',
+                      use_bias=False,
+                      activation=None,
+                      name=prefix + 'project')(x)
+    x = layers.BatchNormalization(
+        epsilon=1e-3, momentum=0.999, name=prefix + 'project_BN')(x)
+
+    if in_channels == pointwise_filters and stride == 1:
+        return layers.Add(name=prefix + 'add')([inputs, x])
+    return x
+
+def unet_classic(input_tensor, num_classes):
+    conv1 = Conv2D(64, 3, activation='relu', padding='same',
+                   kernel_initializer='he_normal')(input_tensor)
+    conv1 = Conv2D(64, 3, activation='relu', padding='same',
+                   kernel_initializer='he_normal')(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+
+    conv2 = Conv2D(128, 3, activation='relu', padding='same',
+                   kernel_initializer='he_normal')(pool1)
+    conv2 = Conv2D(128, 3, activation='relu', padding='same',
+                   kernel_initializer='he_normal')(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+
+    conv3 = Conv2D(256, 3, activation='relu', padding='same',
+                   kernel_initializer='he_normal')(pool2)
+    conv3 = Conv2D(256, 3, activation='relu', padding='same',
+                   kernel_initializer='he_normal')(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+
+    conv4 = Conv2D(512, 3, activation='relu', padding='same',
+                   kernel_initializer='he_normal')(pool3)
+    conv4 = Conv2D(512, 3, activation='relu', padding='same',
+                   kernel_initializer='he_normal')(conv4)
+    drop4 = Dropout(0.5)(conv4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
+
+    conv5 = Conv2D(1024, 3, activation='relu', padding='same',
+                   kernel_initializer='he_normal')(pool4)
+    conv5 = Conv2D(1024, 3, activation='relu', padding='same',
+                   kernel_initializer='he_normal')(conv5)
+    drop5 = Dropout(0.5)(conv5)
+
+    up6 = Conv2D(512, 2, activation='relu', padding='same',
+                 kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(drop5))
+    merge6 = concatenate([drop4, up6], axis=-1)
+    conv6 = Conv2D(512, 3, activation='relu', padding='same',
+                   kernel_initializer='he_normal')(merge6)
+    conv6 = Conv2D(512, 3, activation='relu', padding='same',
+                   kernel_initializer='he_normal')(conv6)
+
+    up7 = Conv2D(256, 2, activation='relu', padding='same',
+                 kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(conv6))
+    merge7 = concatenate([conv3, up7], axis=-1)
+    conv7 = Conv2D(256, 3, activation='relu', padding='same',
+                   kernel_initializer='he_normal')(merge7)
+    conv7 = Conv2D(256, 3, activation='relu', padding='same',
+                   kernel_initializer='he_normal')(conv7)
+
+    up8 = Conv2D(128, 2, activation='relu', padding='same',
+                 kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(conv7))
+    merge8 = concatenate([conv2, up8], axis=-1)
+    conv8 = Conv2D(128, 3, activation='relu', padding='same',
+                   kernel_initializer='he_normal')(merge8)
+    conv8 = Conv2D(128, 3, activation='relu', padding='same',
+                   kernel_initializer='he_normal')(conv8)
+
+    up9 = Conv2D(64, 2, activation='relu', padding='same',
+                 kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(conv8))
+    merge9 = concatenate([conv1, up9], axis=-1)
+    conv9 = Conv2D(64, 3, activation='relu', padding='same',
+                   kernel_initializer='he_normal')(merge9)
+    conv9 = Conv2D(64, 3, activation='relu', padding='same',
+                   kernel_initializer='he_normal')(conv9)
+    conv9 = Conv2D(2, 3, activation='relu', padding='same',
+                   kernel_initializer='he_normal')(conv9)
+    conv10 = Conv2D(num_classes, 1, activation='sigmoid', name='output_1')(conv9)
+
+    return conv10
+
+def load_backbone_weights(model, image_size):
+    pass
+
+network = unet_classic
+
+if __name__ == "__main__":
+    from keras.layers import Input
+    inputs = Input([224, 224, 3], name='input_1')
+    net = network(inputs, 3)
+
+    from keras.models import Model
+
+    model = Model(inputs, net)
+
+    model.summary()

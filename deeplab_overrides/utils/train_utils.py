432db766dd50c2d15d79edce3bf0490b89ce92f1
Sercan Turkmen <sercanturkmen@outlook.com>
2019-01-07 07:15:30 +0200

    change smooth value

--- deeplab_overrides/utils/train_utils.py
+++ deeplab_overrides/utils/train_utils.py
@@ -85,7 +85,7 @@ def add_softmax_cross_entropy_loss_for_each_scale(scales_to_logits,
 
         if add_jaccard_coef:
             tf.losses.add_loss(
-                1.0 - dice_coefficient(softmax_logits, one_hot_labels))
+                1.0 - dice_coefficient(softmax_logits, one_hot_labels, smooth=1.))
 
 
 get_model_init_fn = _super.get_model_init_fn

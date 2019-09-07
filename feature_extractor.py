2fe1e2df79393ca624a5c323ebdefe60be3a8e0e
Sercan Turkmen <sercanturkmen@outlook.com>
2019-09-07 19:21:26 +0300

    fix depthwise convolutions

--- feature_extractor.py
+++ feature_extractor.py
@@ -8,7 +8,7 @@ from tensorflow.keras.layers import (
     Conv2D,
     BatchNormalization,
     MaxPool2D,
-    SeparableConv2D,
+    DepthwiseConv2D,
     Concatenate,
     Reshape,
     Permute,
@@ -63,8 +63,8 @@ def basic_unit_with_downsampling(
         kernel_regularizer=keras.regularizers.l2(WEIGHT_DECAY),
     )(inputs)
     right_path = batch_norm(right_path)
-    right_path = SeparableConv2D(
-        in_channels, kernel_size=3, strides=stride, dilation_rate=rate, padding="same"
+    right_path = DepthwiseConv2D(
+        kernel_size=3, strides=stride, dilation_rate=rate, padding="same"
     )(right_path)
     right_path = batch_norm(right_path)
     right_path = Conv2D(
@@ -78,8 +78,8 @@ def basic_unit_with_downsampling(
     right_path = batch_norm(right_path)
 
     # left path
-    left_path = SeparableConv2D(
-        in_channels, kernel_size=3, strides=stride, dilation_rate=rate, padding="same"
+    left_path = DepthwiseConv2D(
+        kernel_size=3, strides=stride, dilation_rate=rate, padding="same"
     )(inputs)
     left_path = batch_norm(left_path)
     left_path = Conv2D(
@@ -115,7 +115,7 @@ def concat_shuffle(inputs: list):
 
 
 def basic_unit(inputs: tf.Tensor, rate: int = 1):
-    splits = Lambda(lambda x: tf.split(x, num_or_size_splits=2, axis=3))(inputs)
+    splits = tf.split(inputs, num_or_size_splits=2, axis=3)
 
     in_channels = splits[0].shape[-1]
 
@@ -128,9 +128,7 @@ def basic_unit(inputs: tf.Tensor, rate: int = 1):
         kernel_regularizer=keras.regularizers.l2(WEIGHT_DECAY),
     )(splits[1])
     x = batch_norm(x)
-    x = SeparableConv2D(
-        in_channels, kernel_size=3, strides=1, dilation_rate=rate, padding="same"
-    )(x)
+    x = DepthwiseConv2D(kernel_size=3, strides=1, dilation_rate=rate, padding="same")(x)
     x = Conv2D(
         in_channels,
         kernel_size=1,
@@ -180,56 +178,58 @@ def shufflenet_v2(inputs: tf.Tensor, num_classes: int, depth_multiplier: float):
         kernel_regularizer=keras.regularizers.l2(WEIGHT_DECAY),
     )(x)
     x = layers.GlobalAveragePooling2D()(x)
-    x = layers.Dense(num_classes)(x)
+    x = layers.Dense(num_classes, activation="softmax", kernel_initializer="he_normal")(
+        x
+    )
 
     return x
 
 
 if __name__ == "__main__":
-    tf.random.set_seed(22)
+    # tf.random.set_seed(22)
 
-    # The data, split between train and test sets:
-    (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
-    print("x_train shape:", x_train.shape)
-    print(x_train.shape[0], "train samples")
-    print(x_test.shape[0], "test samples")
+    # # The data, split between train and test sets:
+    # (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
+    # print("x_train shape:", x_train.shape)
+    # print(x_train.shape[0], "train samples")
+    # print(x_test.shape[0], "test samples")
 
-    # Convert class vectors to binary class matrices.
-    y_train = keras.utils.to_categorical(y_train, 10)
-    y_test = keras.utils.to_categorical(y_test, 10)
+    # # Convert class vectors to binary class matrices.
+    # y_train = keras.utils.to_categorical(y_train, 10)
+    # y_test = keras.utils.to_categorical(y_test, 10)
 
-    x_train = x_train.astype("float32")
-    x_test = x_test.astype("float32")
-    x_train /= 255
-    x_test /= 255
+    # x_train = x_train.astype("float32")
+    # x_test = x_test.astype("float32")
+    # x_train /= 255
+    # x_test /= 255
 
-    input_shape = (32, 32, 3)
-    inputs = keras.Input(shape=input_shape)
+    # input_shape = (32, 32, 3)
+    # inputs = keras.Input(shape=input_shape)
 
-    output = shufflenet_v2(inputs, 10, 1.0)
-    model = tf.keras.Model(inputs=inputs, outputs=output)
-
-    model.compile(loss="categorical_crossentropy", optimizer='adam', metrics=["accuracy"])
-
-    model.fit(
-        x_train,
-        y_train,
-        batch_size=128,
-        epochs=10,
-        validation_data=(x_test, y_test),
-        shuffle=True,
-    )
-
-    scores = model.evaluate(x_test, y_test, verbose=1)
-    print("Test loss:", scores[0])
-    print("Test accuracy:", scores[1])
-
-    model.save("model.h5")
-
-    # inputs = keras.Input(shape=(224, 224, 3))
-    # output = shufflenet_v2(inputs, 1000, 1.0)
-
+    # output = shufflenet_v2(inputs, 10, 1.0)
     # model = tf.keras.Model(inputs=inputs, outputs=output)
 
-    # model.summary()
-    # model.save("anan.h5")
+    # model.compile(loss="categorical_crossentropy", optimizer='adam', metrics=["accuracy"])
+
+    # model.fit(
+    #     x_train,
+    #     y_train,
+    #     batch_size=128,
+    #     epochs=10,
+    #     validation_data=(x_test, y_test),
+    #     shuffle=True,
+    # )
+
+    # scores = model.evaluate(x_test, y_test, verbose=1)
+    # print("Test loss:", scores[0])
+    # print("Test accuracy:", scores[1])
+
+    # model.save("model.h5")
+
+    inputs = keras.Input(shape=(224, 224, 3))
+    output = shufflenet_v2(inputs, 1000, 1.0)
+
+    model = tf.keras.Model(inputs=inputs, outputs=output)
+
+    model.summary()
+    model.save("shufflenet_v2.h5")
